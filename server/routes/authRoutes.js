ea2e6b6cebcc7d924952dc1c000d990d38020b4d
Edwin <egarciapame@gmail.com>
2024-08-29 00:30:42 -0500

    feat: :beers: Implement `authRoutes.js` for authentication endpoints
    - Created `authRoutes.js` to handle login, registration, and token generation.
    - Added routes for user authentication and token management.
    - Ensured routes are secured and properly handle requests and responses.

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+// Ruta para autenticación de usuarios (login)
+router.post('/login', authController.loginUser);
+
+module.exports = router;
