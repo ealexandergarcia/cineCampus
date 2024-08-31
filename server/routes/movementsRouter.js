cad06eb89b8acf187905560e1bdfc44575a83a1c
Edwin <egarciapame@gmail.com>
2024-08-31 14:01:56 -0500

    feat: ✨update movements routes to handle seat reservation

--- server/routes/movementsRouter.js
+++ server/routes/movementsRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createMovement } = require('../controllers/movementsController');
+const { createMovement, reserveSeats } = require('../controllers/movementsController');
 const authenticate = require('../middleware/authMiddleware');
 
 // Ruta para crear un nuevo movimiento (compra de boletos)
 router.post('/v1/purchase', authenticate, createMovement);
+
+// Ruta para reservar asientos (sin pago inmediato)
+router.post('/v1/reserve', authenticate, reserveSeats);
 
 module.exports = router;
