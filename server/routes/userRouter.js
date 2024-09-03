cf45097c2d7dd4daf59d75a784518de2d8a64beb
Edwin <egarciapame@gmail.com>
2024-09-02 19:50:09 -0500

    feat: ✨ add route for search user  API

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createUser } = require('../controllers/usersController'); // Asegúrate de que la ruta al controlador sea correcta
+const { createUser, getUserDetails } = require('../controllers/usersController'); // Asegúrate de que la ruta al controlador sea correcta
 
 // Ruta para crear un nuevo usuario
 router.post('/register', createUser);
 
+router.get('/:id', getUserDetails);
 module.exports = router;
