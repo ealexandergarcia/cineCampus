1b42817cb26cab4169479978139abfec68faac10
Edwin <egarciapame@gmail.com>
2024-09-02 21:05:29 -0500

    feat: ✨ Add route to search for all users

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, getUserDetails, updateUserRole } = require('../controllers/usersController'); // Asegúrate de que la ruta al controlador sea correcta
+const { createUser, getUserDetails, updateUserRole, listUsers } = require('../controllers/usersController'); // Asegúrate de que la ruta al controlador sea correcta
 
 // Ruta para crear un nuevo usuario
 router.post('/register', createUser);
@@ -9,4 +9,6 @@ router.get('/:id', getUserDetails);
 
 // Ruta para actualizar el rol de un usuario
 router.put('/:userId', updateUserRole);
+
+router.get('/', listUsers);
 module.exports = router;
