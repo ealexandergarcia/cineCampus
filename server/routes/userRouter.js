b080b90128139fafe6c6336194549721baa9132a
Edwin <egarciapame@gmail.com>
2024-09-02 20:42:24 -0500

    feat: ✨ Add route for update user roles API

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, getUserDetails } = require('../controllers/usersController'); // Asegúrate de que la ruta al controlador sea correcta
+const { createUser, getUserDetails, updateUserRole } = require('../controllers/usersController'); // Asegúrate de que la ruta al controlador sea correcta
 
 // Ruta para crear un nuevo usuario
 router.post('/register', createUser);
 
 router.get('/:id', getUserDetails);
+
+// Ruta para actualizar el rol de un usuario
+router.put('/:userId', updateUserRole);
 module.exports = router;
