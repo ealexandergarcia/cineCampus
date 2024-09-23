8c2e5ea2d40d929649541d2e47e63b49e04c88e8
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-09-23 09:01:53 -0500

    feat: :sparkles: test styles for tailwind

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,28 +3,17 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+  <div class="flex flex-col items-center justify-center bg-black">
+    <a href="https://vitejs.dev" target="_blank" class="p-6 transition duration-300 hover:drop-shadow-lg">
+      <img src="/vite.svg" class="h-24" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://vuejs.org/" target="_blank" class="p-6 transition duration-300 hover:drop-shadow-lg">
+      <img src="./assets/vue.svg" class="h-24" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+/* Puedes eliminar el CSS antiguo, ya que hemos adaptado los estilos con Tailwind */
 </style>
