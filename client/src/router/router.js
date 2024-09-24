4a09562532bf7a1bfd59342af98a718d9cb8f703
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-09-24 07:37:56 -0500

    feat: :sparkles: addition of login route

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Home from "../views/Home.vue"
 import CreateAccount from '../views/CreateAccount.vue'
+import Login from '../views/Login.vue'
+
 
 const routes = [
   { path: "/", component: Home },
   { path: '/createAccount', component: CreateAccount },
+  { path: '/login', component: Login },
 ];
 
 const router = createRouter({
