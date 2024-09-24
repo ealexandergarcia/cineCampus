2f4d18db74b801046a07654f62878ab8eb56f096
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-09-24 07:31:36 -0500

    feat: :sparkles: addition of createAcccount route

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,7 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from "vue-router"
+import Home from "../views/Home.vue"
+import CreateAccount from '../views/CreateAccount.vue'
 
-const routes = [{ path: "/", component: Home }];
+const routes = [
+  { path: "/", component: Home },
+  { path: '/createAccount', component: CreateAccount },
+];
 
 const router = createRouter({
   history: createWebHistory(),
