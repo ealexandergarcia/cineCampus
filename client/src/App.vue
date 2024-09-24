1090c079254af7b42abbbf2378aef21e75180428
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-09-24 07:17:00 -0500

    feat: ✨ creation of the app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="bg-[#121212] min-h-screen flex flex-col">
     <Header />
     <main class="flex-grow">
@@ -21,4 +21,26 @@ export default {
 
 <style scoped>
 /* No es necesario CSS adicional aquí, Tailwind maneja el layout */
+</style> -->
+
+
+<template>
+  <div class="bg-[#121212] " id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* Puedes agregar estilos globales aquí */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
