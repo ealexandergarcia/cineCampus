014875bed5299fdeb836f5dbf220f21d0b3ba310
Edwin <egarciapame@gmail.com>
2024-09-24 22:50:41 -0500

    feat: :sparkles: login update for cookie validation

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,18 +1,37 @@
-import { createRouter, createWebHistory } from "vue-router"
-import Home from "../views/Home.vue"
-import CreateAccount from '../views/CreateAccount.vue'
-import Login from '../views/Login.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import CreateAccount from '../views/CreateAccount.vue';
+import Login from '../views/Login.vue';
+import OpenningScreen from "../views/OpenningScreen.vue";
 
 const routes = [
-  { path: "/", component: Home },
+  { path: "/", component: OpenningScreen },
   { path: '/createAccount', component: CreateAccount },
   { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// Guardia de navegación
+router.beforeEach((to, from, next) => {
+  const cookies = document.cookie.split("; ").reduce((acc, cookie) => {
+    const [name, value] = cookie.split("=");
+    acc[name] = value;
+    return acc;
+  }, {});
+
+  const cookieName = "token"; // Cambia esto por el nombre de tu cookie
+
+  // Si el usuario intenta acceder a '/login' y la cookie está presente, redirigir a '/home'
+  if (to.path === '/login' && cookies[cookieName]) {
+    next('/home'); // Redirige al home si ya está autenticado
+  } else {
+    next(); // Permite la navegación a la ruta solicitada
+  }
 });
 
 export default router;
