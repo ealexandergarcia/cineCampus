38bd528a23e57a0af7b1bba1e5af5404585cf92c
Edwin <egarciapame@gmail.com>
2024-09-24 22:51:05 -0500

    feat: :sparkles: change in cookie security

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,28 +2,6 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const User = require('../models/usersModel');
 
-/**
- * loginUser - Authenticates a user and returns a JWT token.
- *
- * @param {Object} req - Express request object.
- * @param {Object} req.body - Request body containing user login credentials.
- * @param {String} req.body.email - Email of the user.
- * @param {String} req.body.password - Password of the user.
- * @param {Object} res - Express response object.
- *
- * @returns {Object} JSON response with a JWT token.
- *
- * @throws {Error} If credentials are missing, user is not found, or password is incorrect.
- *
- * @description
- * 1. Verifies that email and password are provided in the request. If not, responds with a 400 status and an error message.
- * 2. Finds the user by email. If the user is not found, responds with a 401 status and an error message.
- * 3. Compares the provided password with the hashed password stored in the database. If they do not match, responds with a 401 status and an error message.
- * 4. Generates a JWT token with the user's ID and email.
- * 5. Responds with the JWT token or an error message if something goes wrong.
- */
-const bcrypt = require('bcryptjs');
-const User = require('../models/usersModel');
 
 /**
  * loginUser - Authenticates a user and returns a JWT token.
@@ -73,7 +51,7 @@ const loginUser = async (req, res) => {
   
       // Configura la cookie correctamente
       res.cookie('token', token, {
-        httpOnly: true,  // Impide acceso desde JavaScript en el navegador
+        httpOnly: false,  // Impide acceso desde JavaScript en el navegador
         secure: false,   // Cambiar a true en producción (HTTPS)
         maxAge: 60 * 60 * 1000,  // 1 hora
         sameSite: 'Strict' // Protege contra ataques CSRF
