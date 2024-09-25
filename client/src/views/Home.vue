2f6d121480d96840035a8d7f61d2df41e2c3b571
Edwin <egarciapame@gmail.com>
2024-09-24 22:51:56 -0500

    feat: :sparkles: assembly of components for homepage.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,43 +1,24 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen ">
-    <img src="../assets/img/Illustration.svg" alt="Image" class="w-3/4 mb-8" />
-    
-    <!-- Texto entre la imagen y los botones -->
-    <div class="text-center mb-8 px-6">
-      <h2 class=" text-white font-semibold mb-2 text-4xl">Explore the app</h2>
-      <p class="text-white">Now your finances are in one place and always under control</p>
-    </div>
-
-    <div class="flex flex-col space-y-4 w-full px-6">
-      <!-- Botón de Log In -->
-      <button @click="goToLogin" class="w-full bg-[#FE0000] text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Log In</button>
-      <!-- Botón de Create Account con colores opuestos -->
-      <button @click="goToCreateAccount" class="w-full bg-white text-black p-2 rounded-md border border-black hover:bg-gray-100 focus:outline-none focus:bg-white focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 transition-colors duration-300">Create Account</button>
-    </div>
+  <div class="bg-[#121212] min-h-screen flex flex-col">
+    <Header />
+    <main class="flex-grow">
+        <NowPlayingCarousel />
+    </main>
+    <Footer />
   </div>
 </template>
 
 <script>
-import { useRouter } from 'vue-router'
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+import NowPlayingCarousel from "../components/NowPlayingCarousel.vue";
+// import MovieList from './components/MovieList.vue'
 
 export default {
-  name:"Home",
-  setup() {
-    const router = useRouter()
-
-    function goToLogin() {
-      router.push('/login')
-    }
-
-    function goToCreateAccount() {
-      router.push('/createAccount')
-    }
-
-    return { goToLogin, goToCreateAccount }
-  }
+  components: { Header, Footer, NowPlayingCarousel }
 }
 </script>
 
 <style scoped>
-/* Puedes agregar estilos específicos si es necesario */
+/* No es necesario CSS adicional aquí, Tailwind maneja el layout */
 </style>
