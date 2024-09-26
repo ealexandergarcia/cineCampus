b221058f7d160f256a9842bf70e4c026c91f6a3f
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-09-26 17:36:48 -0500

    feat: ✨ update of the film model

--- server/models/moviesModel.js
+++ server/models/moviesModel.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 
+// Definición del esquema para un actor
+const actorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  photo: {
+    type: String,
+    required: true // URL de la foto del actor
+  }
+});
+
+// Definición del esquema principal para una película
 const movieSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -17,14 +34,20 @@ const movieSchema = new mongoose.Schema({
     type: Number,
     required: true
   },
-  poster:{
+  poster: {
     type: String,
+  },
+  synopsis: {
+    type: String,
+    required: true // Sinopsis de la película
   },
   status: {
     type: String,
     enum: ['nowPlaying', 'comingSoon', 'expired'],
     default: 'comingSoon'
-}
+  },
+  cast: [actorSchema] // Campo para el reparto
 });
 
+// Exportar el modelo de película
 module.exports = mongoose.model('Movie', movieSchema);
