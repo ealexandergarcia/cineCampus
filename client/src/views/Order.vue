4cbae2de31adeb78d1b8af9e6a67efd002676bd1
Edwin <egarciapame@gmail.com>
2024-09-29 12:03:56 -0500

    feat: :sparkles: update of dates and number of seats purchased

--- client/src/views/Order.vue
+++ client/src/views/Order.vue
@@ -21,12 +21,17 @@
       <p class="text-[13px] text-gray-400">
         ORDER NUMBER: <span class="text-white">{{ orderId }}</span>
       </p>
+      <p class="text-[13px] text-gray-400">
+        
+      </p>
     </div>
-
-
 
     <div class="px-4 space-y-2">
       <div class="pt-6 pb-4 flex justify-between text-gray-400 border-b border-gray-400" v-for="(group, index) in groupedSeats" :key="index">
+        <span>{{ ticketQuantity }}</span>
+        <span>{{ ticketDetailsSeatNames }}</span>
+      </div>
+      <div class="py-4 flex justify-between text-gray-400 border-b border-gray-400" v-for="(group, index) in groupedSeats" :key="index">
         <span>{{ group.type.toUpperCase() }} SEAT</span>
         <span>${{ group.price }} x {{ group.count }}</span>
       </div>
@@ -66,8 +71,8 @@
 </template>
 
 <script>
-import { ref, onMounted, onUnmounted } from 'vue'
-import CinemaHeader from '../components/CinemaHeader.vue'
+import { ref, onMounted, onUnmounted } from 'vue';
+import CinemaHeader from '../components/CinemaHeader.vue';
 
 export default {
   components: { CinemaHeader },
@@ -81,119 +86,129 @@ export default {
       orderId: null,
       selectedSeats: [],
       groupedSeats: [],
-      timeLeft: 300
-    }
+      timeLeft: 300,
+    };
   },
   computed: {
     formattedDate() {
-    if (this.showingTime) {
-      console.log(this.showingTime);
-      
-      // Convertir la cadena de fecha a un objeto Date
-      const date = new Date(this.showingTime);
-      console.log(date);
-      
-      // Obtener fecha en formato deseado
-      const formattedDate = date.toLocaleDateString('en-US', { 
-        weekday: 'short', 
-        day: 'numeric', 
-        month: 'short', 
-        year: 'numeric' 
-      });
-
-      // Obtener hora en formato 24 horas (13:00)
-      const formattedTime = date.toLocaleTimeString('en-US', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false
-      });
-
-      return `${formattedDate}. ${formattedTime}`;
-    }
-    return '';
-  },
+      if (this.showingTime) {
+        const date = new Date(this.showingTime);
+        const formattedDate = date.toLocaleDateString('en-US', { 
+          weekday: 'short', 
+          day: 'numeric', 
+          month: 'short', 
+          year: 'numeric' 
+        });
+        const formattedTime = date.toLocaleTimeString('en-US', {
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        });
+        return `${formattedDate}. ${formattedTime}`;
+      }
+      return '';
+    },
     formattedTimeLeft() {
-      const minutes = Math.floor(this.timeLeft / 60)
-      const seconds = this.timeLeft % 60
-      return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
-    }
+      const minutes = Math.floor(this.timeLeft / 60);
+      const seconds = this.timeLeft % 60;
+      return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    },
+    ticketQuantity() {
+      const count = this.selectedSeats.length;
+      const seatNames = this.selectedSeats.join(', ');
+      return `${count} TICKET${count !== 1 ? 'S' : ''}`;
+    },
+    ticketDetailsSeatNames() {
+      const seatNames = this.selectedSeats.join(', ');
+      return `${seatNames}`;
+    },
   },
   methods: {
+    getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(";").shift();
+    },
     async fetchMovementDetails() {
       try {
-        const movementId = sessionStorage.getItem('movementId')
-        const response = await fetch(`http://localhost:5000/movements/v1/${movementId}`)
-        const data = await response.json()
-        const movement = data.movement
+        const movementId = sessionStorage.getItem('movementId');
+        const token = this.getCookie("token");
+        const response = await fetch(`http://localhost:5000/movements/v1/${movementId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        const movement = data.movement;
 
         // Guardar los datos del movimiento
-        this.orderId = movement._id
-        this.roomPrice = movement.room.price
-        this.selectedSeats = movement.seats ? JSON.parse(movement.seats[0]) : []
-        this.showingTime = movement.showing.datetime
+        this.orderId = movement._id;
+        this.roomPrice = movement.room.price;
+        this.selectedSeats = movement.seats ? JSON.parse(movement.seats[0]) : [];
+        this.showingTime = movement.showing.datetime;
         // Obtener el movieId y realizar la solicitud de detalles de la película
-        const movieId = movement.showing.movie
-        await this.fetchMovieDetails(movieId)
+        const movieId = movement.showing.movie;
+        await this.fetchMovieDetails(movieId);
 
         // Obtener el tiempo y la agrupación de los asientos
-        this.showingTime = movement.showing.datetime
-        this.groupSeats(movement.showing.availableSeats)
+        this.showingTime = movement.showing.datetime;
+        this.groupSeats(movement.showing.availableSeats);
       } catch (error) {
-        console.error('Error fetching movement details:', error)
+        console.error('Error fetching movement details:', error);
       }
     },
-
     async fetchMovieDetails(movieId) {
       try {
-        const response = await fetch(`http://localhost:5000/movies/v1/${movieId}`)
-        const movieData = await response.json()
+        const response = await fetch(`http://localhost:5000/movies/v1/${movieId}`);
+        const movieData = await response.json();
 
         // Guardar los datos de la película
-        this.movieTitle = movieData.title
-        this.moviePoster = movieData.poster
-        this.movieGenre = Array.isArray(movieData.genre) ? movieData.genre.join(', ') : ''
+        this.movieTitle = movieData.title;
+        this.moviePoster = movieData.poster;
+        this.movieGenre = Array.isArray(movieData.genre) ? movieData.genre.join(', ') : '';
       } catch (error) {
-        console.error('Error fetching movie details:', error)
+        console.error('Error fetching movie details:', error);
       }
     },
-
     groupSeats(availableSeats) {
-      const seatGroups = {}
+      const seatGroups = {};
 
       this.selectedSeats.forEach(seatName => {
-        const seat = availableSeats.find(s => s.name === seatName)
+        const seat = availableSeats.find(s => s.name === seatName);
         if (seat) {
-          const key = `${seat.type}-${seat.price}`
+          const key = `${seat.type}-${seat.price}`;
           if (!seatGroups[key]) {
             seatGroups[key] = {
               type: seat.type,
               price: seat.price,
-              count: 0
-            }
+              count: 0,
+            };
           }
-          seatGroups[key].count++
+          seatGroups[key].count++;
         }
-      })
+      });
 
-      this.groupedSeats = Object.values(seatGroups)
-    }
+      this.groupedSeats = Object.values(seatGroups);
+    },
   },
   mounted() {
-    this.fetchMovementDetails()
+    this.fetchMovementDetails();
 
     const timer = setInterval(() => {
       if (this.timeLeft > 0) {
-        this.timeLeft--
+        this.timeLeft--;
       } else {
-        clearInterval(timer)
+        clearInterval(timer);
       }
-    }, 1000)
+    }, 1000);
 
     onUnmounted(() => {
-      clearInterval(timer)
-    })
-  }
-}
+      clearInterval(timer);
+    });
+  },
+};
 </script>
 
 <style scoped>
