7ad1f838a38675e9756367e9da840ebb3c336ac1
Edwin <egarciapame@gmail.com>
2024-09-29 23:46:09 -0500

    feat: :sparkles: update of loading screen styles

--- client/src/components/LoadingScreen.vue
+++ client/src/components/LoadingScreen.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
-    <div class="text-white text-2xl font-semibold">In process...</div>
+    <div class="loader"></div>
+    <div class="text-white text-2xl font-semibold mt-4">Transaction in process...</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'LoadingScreen',
-
   mounted() {
-    // Ejecutar la función después de 8 segundos
     setTimeout(() => {
       this.processPayment();
-    }, 8000);
+    }, 5000);
   },
   methods: {
     getCookie(name) {
@@ -21,17 +20,11 @@ export default {
       if (parts.length === 2) return parts.pop().split(";").shift();
     },
     async processPayment() {
-      const body = {
-        status: "purchased"
-      };
-      console.log(body);
+      const body = { status: "purchased" };
       const token = this.getCookie("token");
-      console.log(token);
       
       try {
         const paymentId = sessionStorage.getItem('paymentId');
-        console.log(paymentId);
-
         const response = await fetch(`http://localhost:5000/payments/${paymentId}/status`, {
           method: 'PUT',
           headers: {
@@ -41,15 +34,11 @@ export default {
           body: JSON.stringify(body),
         });
 
-        if (!response.ok) {
-          throw new Error('Error al procesar el pago');
-        }
-
+        if (!response.ok) throw new Error('Error al procesar el pago');
+        
         const result = await response.json();
         alert('Pago procesado con éxito: ' + result.message);
-
-        // Redirigir a la ruta '/home' después de una petición exitosa
-        this.$router.push('/home');
+        this.$router.push('/Ticket');
       } catch (error) {
         console.error('Error en la compra:', error);
         alert('Error en la compra: ' + error.message);
@@ -60,5 +49,21 @@ export default {
 </script>
 
 <style scoped>
-/* Puedes ajustar los estilos si lo deseas, pero esto mantiene el diseño básico */
+.loader {
+  border: 8px solid #FE0000; /* Cambia los colores según tu proyecto */
+  border-top: 8px solid #ffffff; /* Color del spinner */
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+.text-white {
+  color: #ffffff; /* Cambia el color del texto si es necesario */
+}
 </style>
